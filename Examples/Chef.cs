bfb5aeed4f915dedba6707c358c01a99eeaf05c0
Ana Alarcon Figueroa <Ana.AlarconFigueroa@edu.sait.ca>
2023-01-30 15:53:07 -0700

    first commit
    first commit

---
+++ Examples/Chef.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    internal class Chef
+    {
+        //METHODS
+        public void MakeChicken()
+        {
+            Console.WriteLine("The chef makes chicken");
+        }
+
+        public void MakeSalad()
+        {
+            Console.WriteLine("The chef makes Salad");
+        }
+
+        public void MakeSpecialDish()
+        {
+            Console.WriteLine("The chef makes special dish");
+        }
+    }
+}
